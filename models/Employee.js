9adc842333d96b707fa5ae368f698787c83da78f
HangCcz <hangchen1128@gmail.com>
2022-02-11 00:23:36 -0500

    added timestamps to model

--- models/Employee.js
+++ models/Employee.js
@@ -1,13 +1,16 @@
 import mongoose from 'mongoose'
 
-const EmployeeSchema = new mongoose.Schema({
-  name: String,
-  emailAddress: String,
-  picture_url: String,
-  jobTitle: String,
-  department: String,
-  location: String,
-})
+const EmployeeSchema = new mongoose.Schema(
+  {
+    name: String,
+    emailAddress: String,
+    picture_url: String,
+    jobTitle: String,
+    department: String,
+    location: String,
+  },
+  { timestamps: true }
+)
 
 export default mongoose.models.Employee ||
   mongoose.model('Employee', EmployeeSchema)
