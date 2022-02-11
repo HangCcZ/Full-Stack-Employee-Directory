7e820077c8804a94173ab6e752d75089ec424753
HangCcz <hangchen1128@gmail.com>
2022-02-10 20:07:43 -0500

    added Employee model

--- models/Employee.js
+++ models/Employee.js
@@ -3,8 +3,11 @@ import mongoose from 'mongoose'
 const EmployeeSchema = new mongoose.Schema({
   name: String,
   emailAddress: String,
-  pictures: String,
+  picture_url: String,
   jobTitle: String,
   department: String,
   location: String,
 })
+
+export default mongoose.models.Employee ||
+  mongoose.model('Employee', EmployeeSchema)
