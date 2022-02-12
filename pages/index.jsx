a82588246d186607b22b13db65b8e42840ef9611
HangCcz <hangchen1128@gmail.com>
2022-02-11 23:30:45 -0500

    added UI layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,24 @@
+import useSWR from 'swr'
 import Head from 'next/head'
+import SearchBar from '../components/SearchBar'
+import EmployeeTable from '../components/EmployeeTable'
+import PaginationGroup from '../components/PaginationGroup'
+import CreateForm from '../components/CreateForm'
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 export default function Home() {
+  const { data, error } = useSWR('./api/employees', fetcher)
+
+  if (error) {
+    //TODO: Push to 404
+    return <div></div>
+  }
+  if (!data & !error) {
+    //TODO: Add loading animate
+    return <div>Loading...</div>
+  }
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -8,7 +26,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center"></main>
+      <main className="mt-10 flex w-full max-w-5xl flex-1 flex-col items-center justify-center px-20 text-center">
+        <SearchBar />
+        <EmployeeTable employees={data.data} />
+        <PaginationGroup />
+        <CreateForm />
+      </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
