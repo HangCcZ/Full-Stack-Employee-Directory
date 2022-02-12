c59d0c9389c05a46a6c6d942e91ef10c9ecbee92
HangCcz <hangchen1128@gmail.com>
2022-02-12 13:19:28 -0500

    created a new page to render form

---
+++ pages/new.jsx
@@ -0,0 +1,13 @@
+import useSWR from 'swr'
+import Head from 'next/head'
+import CreateForm from '../components/CreateForm'
+
+export default function New({ employeeInfo }) {
+  return (
+    <>
+      <main className="w-11/12 max-w-5xl flex-1 flex-col py-4 px-2 text-center sm:px-20">
+        <CreateForm />
+      </main>
+    </>
+  )
+}
