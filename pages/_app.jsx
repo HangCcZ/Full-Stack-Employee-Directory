027ae69f8c7e8b067e8facbd9ca8a1f8d2f9120a
HangCcz <hangchen1128@gmail.com>
2022-02-12 13:20:39 -0500

    added header and container

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
-
+import Head from 'next/head'
+import NavBar from '../components/NavBar'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Employee Directory - Postlight</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex flex-col items-center justify-center bg-gray-100 py-4">
+        <NavBar />
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
 
 export default MyApp
