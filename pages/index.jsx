c0bee21a190867b6aacc6d8ec8213dfbf8b96ebd
HangCcz <hangchen1128@gmail.com>
2022-02-12 13:20:14 -0500

    removed footer and lifted header and container to _app

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,30 +20,15 @@ export default function Home() {
   }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Employee Directory - Postlight</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="mt-10 flex w-full max-w-5xl flex-1 flex-col items-center justify-center px-20 text-center">
+    <>
+      <main className="w-11/12 max-w-5xl flex-1 flex-col py-4 px-2 text-center sm:px-20">
         <SearchBar />
         <EmployeeTable employees={data.data} />
+        {/**TODO:
+         * This PaginationGroup constrains the whole container,
+         * NarBar is expanded if PaginationGroup is not included*/}
         <PaginationGroup />
-        <CreateForm />
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-        </a>
-      </footer>
-    </div>
+    </>
   )
 }
