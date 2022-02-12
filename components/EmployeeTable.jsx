15d870b70cb3b34d17bfb3ccb1b2e39b080c075b
HangCcz <hangchen1128@gmail.com>
2022-02-12 13:23:07 -0500

    hidden two th element which caused problems in mobile view, added code to constrain td size when data is too long

--- components/EmployeeTable.jsx
+++ components/EmployeeTable.jsx
@@ -1,7 +1,8 @@
 export default function EmployeeTable({ employees }) {
+  const charLimit = 25
   return (
     <div className="mt-6 flex flex-col">
-      <div className="-my-2 overflow-x-hidden sm:-mx-6 lg:-mx-8">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
           <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
             <table className="min-w-full divide-y divide-gray-200">
@@ -25,11 +26,13 @@ export default function EmployeeTable({ employees }) {
                   >
                     DEPARTMENT
                   </th>
+                  {/**span need to be hidden or it will occupy spaces and destory the mobile view */}
                   <th scope="col">
-                    <span className="sr-only">View More</span>
+                    <span className="sr-only hidden">View More</span>
                   </th>
+                  {/**span need to be hidden or it will occupy spaces and destory the mobile view*/}
                   <th scope="col">
-                    <span className="sr-only">Edit</span>
+                    <span className="sr-only hidden">Edit</span>
                   </th>
                 </tr>
               </thead>
@@ -55,14 +58,16 @@ export default function EmployeeTable({ employees }) {
                         </div>
                       </div>
                     </td>
-                    <td className="whitespace-nowrap px-6 py-4">
-                      <div className="text-sm text-gray-600">
-                        {employee.title}
-                      </div>
+                    <td className="w-10 whitespace-nowrap px-6 py-4 text-left text-sm text-gray-600">
+                      {employee.title.length > charLimit
+                        ? employee.title.substring(0, charLimit) + '...'
+                        : employee.title}
                     </td>
 
-                    <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
-                      {employee.department}
+                    <td className="whitespace-nowrap px-6 py-4 text-left text-sm text-gray-500">
+                      {employee.department.length > charLimit
+                        ? employee.department.substring(0, charLimit) + '...'
+                        : employee.department}
                     </td>
                     <td className="whitespace-nowrap px-6 py-4 text-right text-sm font-medium">
                       <a href="#" className="text-blue-600 hover:text-blue-900">
