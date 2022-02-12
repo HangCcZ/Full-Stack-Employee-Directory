136e681183013f00670ab6fc91e9b5fc72bee1aa
HangCcz <hangchen1128@gmail.com>
2022-02-12 13:21:44 -0500

    added NarBar to share across pages

---
+++ components/NavBar.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+export default function NavBar() {
+  return (
+    <div className="flex w-11/12 max-w-5xl justify-between px-2 text-center sm:px-20">
+      <Link href="/">
+        <a className="p-2 font-semibold text-gray-600 hover:rounded-lg hover:bg-blue-500 hover:text-gray-50 active:bg-blue-200">
+          Home
+        </a>
+      </Link>
+      <Link href="/new">
+        <a className="p-2 font-semibold text-gray-600 hover:rounded-lg hover:bg-blue-500 hover:text-gray-50 active:bg-blue-200">
+          Add Employee
+        </a>
+      </Link>
+    </div>
+  )
+}
