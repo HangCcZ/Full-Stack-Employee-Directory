4510cf10471812e7ae32c8f3c3904361a4483bae
HangCcz <hangchen1128@gmail.com>
2022-02-13 14:43:20 -0500

    added props to select edit/new form

--- pages/new.jsx
+++ pages/new.jsx
@@ -2,11 +2,11 @@ import useSWR from 'swr'
 import Head from 'next/head'
 import CreateForm from '../components/CreateForm'
 
-export default function New({ employeeInfo }) {
+export default function New() {
   return (
     <>
       <main className="w-11/12 max-w-5xl flex-1 flex-col py-4 px-2 text-center sm:px-20">
-        <CreateForm />
+        <CreateForm preloaded={{}} isNew={true} />
       </main>
     </>
   )
