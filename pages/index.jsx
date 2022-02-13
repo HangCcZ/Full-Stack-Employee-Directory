de50ab8d4158f3729a8191422905f885a15c878e
HangCcz <hangchen1128@gmail.com>
2022-02-13 00:05:29 -0500

    added error handling when fetcher failed, added comments

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,9 @@
 import useSWR from 'swr'
-import Head from 'next/head'
 import SearchBar from '../components/SearchBar'
 import EmployeeTable from '../components/EmployeeTable'
 import PaginationGroup from '../components/PaginationGroup'
 import CreateForm from '../components/CreateForm'
+import { useRouter } from 'next/router'
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
@@ -11,11 +11,10 @@ export default function Home() {
   const { data, error } = useSWR('./api/employees', fetcher)
 
   if (error) {
-    //TODO: Push to 404
-    return <div></div>
+    useRouter.push('/')
   }
   if (!data & !error) {
-    //TODO: Add loading animate
+    //TODO - minor issue: Add loading animate
     return <div>Loading...</div>
   }
 
@@ -24,7 +23,7 @@ export default function Home() {
       <main className="w-11/12 max-w-5xl flex-1 flex-col py-4 px-2 text-center sm:px-20">
         <SearchBar />
         <EmployeeTable employees={data.data} />
-        {/**TODO:
+        {/**TODO - minor issue:
          * This PaginationGroup constrains the whole container,
          * NarBar is expanded if PaginationGroup is not included*/}
         <PaginationGroup />
