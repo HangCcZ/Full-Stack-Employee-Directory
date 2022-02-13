6f81fb512ca5f1ea848110231dd8b5b9afae5591
HangCcz <hangchen1128@gmail.com>
2022-02-13 17:21:10 -0500

    moved style to lower level component

--- pages/new.jsx
+++ pages/new.jsx
@@ -5,7 +5,7 @@ import CreateForm from '../components/CreateForm'
 export default function New() {
   return (
     <>
-      <main className="w-11/12 max-w-5xl flex-1 flex-col py-4 px-2 text-center sm:px-20">
+      <main className="w-11/12 max-w-5xl flex-1 flex-col py-4 px-2 sm:px-20">
         <CreateForm preloaded={{}} isNew={true} />
       </main>
     </>
