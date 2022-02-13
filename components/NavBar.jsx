9cf514c835b80c0ad6103b869529d0dab539b598
HangCcz <hangchen1128@gmail.com>
2022-02-13 17:22:03 -0500

    removed horizontal padding

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function NavBar() {
   return (
-    <div className="flex w-11/12 max-w-5xl justify-between px-2 text-center sm:px-20">
+    <div className="flex w-11/12 max-w-5xl justify-between px-2 text-center">
       <Link href="/">
         <a className="p-2 font-semibold text-gray-600 hover:rounded-lg hover:bg-blue-500 hover:text-gray-50 active:bg-blue-200">
           Home
