77a5e66345980087363233bf66dd64f3181c1dc7
HangCcz <hangchen1128@gmail.com>
2022-02-13 23:06:16 -0500

    added DeletionModal to handle delete action

---
+++ components/DeletionModal.jsx
@@ -0,0 +1,135 @@
+import { useState } from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+
+export default function Modal({ id, name }) {
+  const [showModal, setShowModal] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+  const handleDelete = async () => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:3000/api/employees/${id}`
+      )
+      console.log(response)
+      if (response.status == 201) {
+        setShowModal(false)
+        router.push('/')
+        //TODO: Triggle a success notification
+      }
+    } catch (error) {
+      console.log(error)
+      //TODO: Triggle a fail notification
+      setErrorMessage(
+        () => 'Error occurs when deleting profile, please try again'
+      )
+      setTimeout(() => {
+        setErrorMessage('')
+      }, 2000)
+    }
+  }
+  return (
+    <>
+      <button onClick={() => setShowModal(true)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            className="text-red-400"
+            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+          />
+        </svg>
+      </button>
+      {showModal ? (
+        <>
+          <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
+            <div className="relative my-6 mx-auto w-auto max-w-3xl">
+              {/*content*/}
+              <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between rounded-t border-b border-solid p-5">
+                  <h3 className="text-2xl font-semibold text-red-500">
+                    Profile Deletion
+                  </h3>
+                  {errorMessage && (
+                    <div className="pt-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="inline-block h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          className="text-yellow-400"
+                          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                        />
+                      </svg>
+                      <p className="text-md inline pt-2 text-yellow-500">
+                        {errorMessage}
+                      </p>
+                    </div>
+                  )}
+                  <button
+                    className="float-right p-1 text-3xl font-semibold"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        className="text-gray-800"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative flex-auto p-6">
+                  <p className="my-4 text-lg leading-relaxed text-gray-700">
+                    {`You are deleting ${name}'s profile. Click delete button to
+                    confirm this action.`}
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="border-blueGray-200 flex items-center justify-end rounded-b border-t border-solid p-6">
+                  <button
+                    className="background-transparent mr-1 mb-1 px-6 py-2 text-sm font-bold uppercase text-blue-500 outline-none transition-all duration-150 ease-linear focus:outline-none"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="mr-1 mb-1 rounded bg-red-500 px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-emerald-600"
+                    type="button"
+                    onClick={handleDelete}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+        </>
+      ) : null}
+    </>
+  )
+}
