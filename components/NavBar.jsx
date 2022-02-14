03e2a3c46d0bee43ed15a9f3bdf8561d4c6d836c
HangCcz <hangchen1128@gmail.com>
2022-02-13 22:10:21 -0500

    changed color of Links

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,12 +4,12 @@ export default function NavBar() {
   return (
     <div className="flex w-11/12 max-w-5xl justify-between px-2 text-center">
       <Link href="/">
-        <a className="p-2 font-semibold text-gray-600 hover:rounded-lg hover:bg-blue-500 hover:text-gray-50 active:bg-blue-200">
+        <a className="p-2 font-semibold text-blue-500 hover:rounded-lg hover:bg-blue-500 hover:text-gray-50 active:bg-blue-200">
           Home
         </a>
       </Link>
       <Link href="/new">
-        <a className="p-2 font-semibold text-gray-600 hover:rounded-lg hover:bg-blue-500 hover:text-gray-50 active:bg-blue-200">
+        <a className="p-2 font-semibold text-blue-500 hover:rounded-lg hover:bg-blue-500 hover:text-gray-50 active:bg-blue-200">
           Add Employee
         </a>
       </Link>
