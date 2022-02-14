19223fcdcc96dab034717d26f191c2c524978acc
HangCcz <hangchen1128@gmail.com>
2022-02-13 23:03:44 -0500

    corrected property name

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,8 +17,8 @@ export default function Home() {
     //TODO - minor issue: Add loading animate
     return (
       <div className="flex items-center justify-center">
-        <button type="button" class="bg-indigo-500" disabled>
-          <svg class="mr-3 h-5 w-5 animate-spin" viewBox="0 0 24 24"></svg>
+        <button type="button" className="bg-indigo-500" disabled>
+          <svg className="mr-3 h-5 w-5 animate-spin" viewBox="0 0 24 24"></svg>
           Processing...
         </button>
       </div>
