260cfcc6d63ffd631b5b3302ef8aca92dca1d40a
HangCcz <hangchen1128@gmail.com>
2022-02-15 00:45:37 -0500

    added sticky property

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function NavBar() {
   return (
-    <div className="flex w-11/12 max-w-5xl justify-between px-2 text-center">
+    <div className="sticky top-2 z-50 flex w-11/12 max-w-5xl justify-between px-2 text-center">
       <Link href="/">
         <a className="p-2 font-semibold text-blue-500 hover:rounded-lg hover:bg-blue-500 hover:text-gray-50 active:bg-blue-200">
           Home
