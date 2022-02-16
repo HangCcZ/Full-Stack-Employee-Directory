bb26eecda9cc587eb71a426d756fc7399548e334
HangCcz <hangchen1128@gmail.com>
2022-02-16 11:42:33 -0500

    added dynamic HOST variable for production, changed data variable from form to be more descriptive

--- components/CreateForm.jsx
+++ components/CreateForm.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { mutate } from 'swr'
 import Link from 'next/link'
 import axios from 'axios'
+import { HOST } from '../config'
 
 export default function CreateForm({ preloaded, isNew }) {
   const router = useRouter()
@@ -33,15 +34,14 @@ export default function CreateForm({ preloaded, isNew }) {
    * @param {updated employee data} data
    * @returns
    */
-  const updateEmployee = async (data) => {
+  const updateEmployee = async (formFields) => {
     const { id } = router.query
-    console.log(data.pictureUrl)
-
     // data.pictureUrl will be type of object (FileList) if image is updated
-    if (typeof data.pictureUrl != 'string') {
+
+    if (typeof formFields.pictureUrl != 'string') {
       try {
         const formData = new FormData()
-        for (const file of data.pictureUrl) {
+        for (const file of formFields.pictureUrl) {
           formData.append('file', file)
         }
         formData.append('upload_preset', 'postlight-uploads')
@@ -49,7 +49,7 @@ export default function CreateForm({ preloaded, isNew }) {
           'https://api.cloudinary.com/v1_1/doyzkjrey/image/upload',
           formData
         )
-        data.pictureUrl = cloudinaryData.data.url
+        formFields.pictureUrl = cloudinaryData.data.url
       } catch (error) {
         //TODO: Triggle a fail notification
         return <p>Error when uploading image </p>
@@ -59,11 +59,9 @@ export default function CreateForm({ preloaded, isNew }) {
       /**TODO:
        * Absolute route is used here, need to replace in production
        */
-      const response = await axios.put(
-        `http://localhost:3000/api/employees/${id}`,
-        data
-      )
-      mutate(`http://localhost:3000/api/employees/${id}`, data, false)
+      const { data } = await axios.put(`${HOST}/api/employees/${id}`, data)
+      mutate(`${HOST}/api/employees/${id}`, data.updateEmployee, false)
+
       if (response.status == 201) {
         router.push('/')
         //TODO: Triggle a success notification
@@ -79,9 +77,9 @@ export default function CreateForm({ preloaded, isNew }) {
    * @param {new employee data} data
    * @returns
    */
-  const createEmployee = async (data) => {
+  const createEmployee = async (formFields) => {
     const formData = new FormData()
-    for (const file of data.pictureUrl) {
+    for (const file of formFields.pictureUrl) {
       formData.append('file', file)
     }
     formData.append('upload_preset', 'postlight-uploads')
@@ -92,7 +90,7 @@ export default function CreateForm({ preloaded, isNew }) {
         'https://api.cloudinary.com/v1_1/doyzkjrey/image/upload',
         formData
       )
-      data.pictureUrl = cloudinaryData.data.url
+      formFields.pictureUrl = cloudinaryData.data.url
     } catch (error) {
       //TODO: Triggle a fail notification
       return <p>Error when uploading image </p>
@@ -102,10 +100,7 @@ export default function CreateForm({ preloaded, isNew }) {
       /**TODO:
        * Absolute route is used here, need to replace in production
        */
-      const response = await axios.post(
-        'http://localhost:3000/api/employees/',
-        data
-      )
+      const response = await axios.post(`${HOST}/api/employees/`, data)
       if (response.status == 201) {
         router.push('/')
         //TODO: Triggle a success notification
@@ -118,12 +113,12 @@ export default function CreateForm({ preloaded, isNew }) {
 
   /**
    * Trigger http request based on form's purpose
-   * @param {data}
+   * @param {formFields}
    * @returns
    */
-  const onSubmit = async (data) => {
-    if (!isNew) return updateEmployee(data)
-    else return createEmployee(data)
+  const onSubmit = async (formFields) => {
+    if (!isNew) return updateEmployee(formFields)
+    else return createEmployee(formFields)
   }
 
   return (
@@ -146,7 +141,7 @@ export default function CreateForm({ preloaded, isNew }) {
                 <div className="grid grid-cols-6 gap-6">
                   <div className="col-span-6 sm:col-span-3">
                     <label
-                      htmlFor="first-name"
+                      htmlFor="firstName"
                       className=" block text-sm font-medium text-gray-700"
                     >
                       First name
@@ -155,21 +150,28 @@ export default function CreateForm({ preloaded, isNew }) {
                       className="mt-1 block w-full rounded-md border border-gray-300 px-2 py-2 shadow-sm outline-none focus:border-2 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
                       type="text"
                       name="firstName"
-                      {...register('name', { required: true, maxLength: 80 })}
+                      {...register('firstName', {
+                        required: true,
+                        maxLength: 80,
+                      })}
                     />
                   </div>
 
                   <div className="col-span-6 sm:col-span-3">
                     <label
-                      htmlFor="last-name"
+                      htmlFor="lastName"
                       className="block text-sm font-medium text-gray-700"
                     >
                       Last name
                     </label>
                     <input
+                      className="mt-1 block w-full rounded-md border border-gray-300 px-2 py-2 shadow-sm outline-none focus:border-2 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
                       type="text"
                       name="lastName"
-                      className="mt-1 block w-full rounded-md border border-gray-300 px-2 py-2 shadow-sm outline-none focus:border-2 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      {...register('lastName', {
+                        required: true,
+                        maxLength: 80,
+                      })}
                     />
                   </div>
 
