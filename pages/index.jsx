6c2828b9c49af0d5a2ed2c3189c766124ca68839
HangCcz <hangchen1128@gmail.com>
2022-02-16 11:37:14 -0500

    added code to handle url query

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,37 @@
+import { HOST } from '../config'
 import useSWR from 'swr'
 import SearchBar from '../components/SearchBar'
 import EmployeeTable from '../components/EmployeeTable'
 import PaginationGroup from '../components/PaginationGroup'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 export default function Home() {
+  const [searchValue, setSearchValue] = useState('')
   const router = useRouter()
-  const { data, error } = useSWR(
-    router.query.page
-      ? `./api/employees/?page=${router.query.page}`
-      : `./api/employees`,
-    fetcher
-  )
+
+  /**TODO:
+   * need to handle query that makes no sense, for example /?page=100
+   * when there are only 10 pages total
+   */
+  const { data, error } = useSWR(() => {
+    if (router.query.page && router.query.search) {
+      return `${HOST}/api/employees/?page=${router.query.page}&search=${router.query.search}`
+    } else if (router.query.page) {
+      return `${HOST}/api/employees/?page=${router.query.page}`
+    } else if (router.query.search) {
+      return `${HOST}/api/employees/?search=${router.query.search}`
+    } else {
+      return `${HOST}/api/employees`
+    }
+  }, fetcher)
 
   if (error) {
-    useRouter.push('/')
+    router.push('/')
   }
+
   if (!data & !error) {
     //TODO - minor issue: Add loading animate
     return (
@@ -30,12 +44,14 @@ export default function Home() {
     )
   }
 
+  const { employees, pageData } = data
+
   return (
     <>
       <main className="w-11/12 max-w-5xl flex-1 flex-col py-4 px-2 text-center">
-        <SearchBar />
-        <EmployeeTable employees={data.employees} />
-        <PaginationGroup pageData={data.pageData} />
+        <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
+        <EmployeeTable employees={employees} />
+        <PaginationGroup pageData={pageData} />
       </main>
     </>
   )
