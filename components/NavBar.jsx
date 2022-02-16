ad9b7b61a592eb8e1d71d950961aa96ab020c4ce
HangCcz <hangchen1128@gmail.com>
2022-02-16 11:39:44 -0500

    added dynamic HOST variable for production

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,14 +1,14 @@
 import Link from 'next/link'
-
+import { HOST } from '../config'
 export default function NavBar() {
   return (
-    <div className="sticky top-2 z-50 flex w-11/12 max-w-5xl justify-between px-2 text-center">
-      <Link href="/">
+    <div className="flex w-11/12 max-w-5xl justify-between py-4 px-2 text-center">
+      <Link href={`${HOST}/`}>
         <a className="p-2 font-semibold text-blue-500 hover:rounded-lg hover:bg-blue-500 hover:text-gray-50 active:bg-blue-200">
           Home
         </a>
       </Link>
-      <Link href="/new">
+      <Link href={`${HOST}/new`}>
         <a className="p-2 font-semibold text-blue-500 hover:rounded-lg hover:bg-blue-500 hover:text-gray-50 active:bg-blue-200">
           Add Employee
         </a>
