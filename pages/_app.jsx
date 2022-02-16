bfae1b08813c3a8b629f0fcff6efda668858de0e
HangCcz <hangchen1128@gmail.com>
2022-02-16 11:35:50 -0500

    modified container

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,18 +1,20 @@
 import '../styles/globals.css'
 import Head from 'next/head'
 import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <div className="min-h-screen bg-gray-100">
       <Head>
         <title>Employee Directory - Postlight</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex flex-col items-center justify-center bg-gray-100 py-4">
+      <div className="flex flex-col items-center justify-center bg-gray-100 pb-4">
         <NavBar />
         <Component {...pageProps} />
       </div>
-    </>
+      <Footer />
+    </div>
   )
 }
 
