d72fbf45c56573c9673f74d1580c13b22fedec03
HangCcz <hangchen1128@gmail.com>
2022-02-16 11:43:27 -0500

    added code for production use

---
+++ config.js
@@ -0,0 +1,13 @@
+const env = process.env.NODE_ENV
+let HOST
+
+if (env == 'development') {
+  HOST = process.env.NEXT_PUBLIC_LOCALHOST_NAME
+} else if (env == 'production') {
+  HOST = process.env.NEXT_PUBLIC_PRODUCTION_HOST
+}
+
+console.log(process.env.NEXT_PUBLIC_LOCALHOST_NAME)
+module.exports = {
+  HOST,
+}
