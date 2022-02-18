67a03609f4ea65a9c693c6a4fd2c217bddb6f1de
HangCcz <hangchen1128@gmail.com>
2022-02-17 21:23:57 -0500

    made button disabled when HTTP request is ongoing

--- components/CreateForm.jsx
+++ components/CreateForm.jsx
@@ -288,9 +288,8 @@ export default function CreateForm({ preloaded, isNew }) {
                 </Link>
                 <button
                   type="submit"
-                  className={`${
-                    requestLoading ? `animate-spin` : ''
-                  } ml-4 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
+                  className={`ml-4 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
+                  disabled={requestLoading ? true : false}
                 >
                   {isNew ? 'Create' : 'Save'}
                 </button>
