aad09655794b844c4f3ed6ef0a2e04ae3b41e2d1
HangCcz <hangchen1128@gmail.com>
2022-02-19 01:19:19 -0500

    added ESLINT

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    semi: 'off',
+    'linebreak-style': 0,
+    'react/prop-types': 'off',
+  },
+}
