f37e1c4e53c836972df5948f35519626cb0c758e
HangCcz <hangchen1128@gmail.com>
2022-02-20 18:53:25 -0500

    added code to reset searchValue when home button is clicked

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,12 +1,21 @@
 import Link from 'next/link'
 import { HOST } from '../config'
-export default function NavBar() {
+
+export default function NavBar({ setSearchValue }) {
+  const onHomeClick = () => {
+    setSearchValue(() => '')
+  }
+
   return (
     <div className="flex w-11/12 max-w-5xl justify-between py-4 px-2 text-center">
-      <Link href={`${HOST}/`}>
-        <a className="p-2 font-semibold text-blue-500 hover:rounded-lg hover:bg-blue-500 hover:text-gray-50 active:bg-blue-200">
+      <Link href={`${HOST}/`} passHref>
+        <button
+          type="button"
+          onClick={onHomeClick}
+          className="p-2 font-semibold text-blue-500 hover:rounded-lg hover:bg-blue-500 hover:text-gray-50 active:bg-blue-200"
+        >
           Home
-        </a>
+        </button>
       </Link>
       <Link href={`${HOST}/new`}>
         <a className="p-2 font-semibold text-blue-500 hover:rounded-lg hover:bg-blue-500 hover:text-gray-50 active:bg-blue-200">
