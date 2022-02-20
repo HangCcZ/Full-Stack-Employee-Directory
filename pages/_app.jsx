2af941a44433a629915a4103965f25dc4cc0bd50
HangCcz <hangchen1128@gmail.com>
2022-02-20 18:53:04 -0500

    lifed searchValue state to _app and pass to NavBar as prop

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import { useState } from 'react'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
+
 function MyApp({ Component, pageProps }) {
+  const [searchValue, setSearchValue] = useState('')
   return (
     <div className="min-h-screen bg-gray-100">
       <Head>
@@ -10,8 +13,12 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex flex-col items-center justify-center bg-gray-100 pb-4">
-        <NavBar />
-        <Component {...pageProps} />
+        <NavBar setSearchValue={setSearchValue} />
+        <Component
+          {...pageProps}
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
       </div>
       <Footer />
     </div>
