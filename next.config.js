4788a2c41d6de671d4fecd1a0bf6d6f7bd1aa68a
HangCcz <hangchen1128@gmail.com>
2022-02-19 20:27:25 -0500

    ignore eslintrc

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   async headers() {
     return [
